f69017f23231012406102096a02f732861fa3391
Marco Frassoni <marco.frassoni@gmail.com>
2009-04-06 13:32:45 +0200

    trying to be more tidy (correcting indentation)

--- webroot/js/portal/widget/iframer.js
+++ webroot/js/portal/widget/iframer.js
@@ -1,22 +1,22 @@
 /**
-* This file is part of taolin project (http://taolin.fbk.eu)
-* Copyright (C) 2008, 2009 FBK Foundation, (http://www.fbk.eu)
-* Authors: SoNet Group (see AUTHORS.txt)
-*
-* Taolin is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation version 3 of the License.
-*
-* Taolin is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with Taolin. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
-
+ * This file is part of taolin project (http://taolin.fbk.eu)
+ * Copyright (C) 2008, 2009 FBK Foundation, (http://www.fbk.eu)
+ * Authors: SoNet Group (see AUTHORS.txt)
+ *
+ * Taolin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * Taolin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Taolin. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
 /**
   * Ext.ux.fbk.sonet.Iframer Extension Class
   *
