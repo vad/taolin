b088e14467e2256b559e5f4ed9736e03775253d5
Marco Frassoni <marco.frassoni@gmail.com>
2010-03-09 00:25:30 +0100

    Updating SQL file

--- config/sql/taolin.data.sql
+++ config/sql/taolin.data.sql
@@ -165,4 +165,4 @@ INSERT INTO "widgets_skel" ("id", "widget_id", "col", "pos", "tab") VALUES (4,5,
 
 -- # Populating backgrounds table
 
-INSERT INTO backgrounds VALUES (1, 'Blue curl', 'img/blue_curl.jpeg');
+INSERT INTO backgrounds VALUES (1, 'Blue curl', 'img/background/blue_curl.jpeg');
