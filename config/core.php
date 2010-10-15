4f44e49c467384c077ded782af5e1a9e0d15e3a4
Marco Frassoni <marco.frassoni@gmail.com>
2010-10-15 16:47:35 +0200

    allowing cake sessions to be stored in db

--- config/core.php
+++ config/core.php
@@ -147,14 +147,14 @@
  *
  * [Note: Session.table is deprecated as of CakePHP 1.3]
  */
-    //Configure::write('Session.table', 'cake_sessions');
+    Configure::write('Session.table', 'cake_sessions');
 
 /**
  * The DATABASE_CONFIG::$var to use for database session handling.
  *
  * 'Session.save' must be set to 'database' in order to utilize this constant.
  */
-    //Configure::write('Session.database', 'default');
+    Configure::write('Session.database', 'default');
 
 /**
  * The name of CakePHP's session cookie.
